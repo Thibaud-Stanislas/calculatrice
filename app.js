ee7afaadb9e3784c59b68c86e9de4cf40c3f6485
Thibaud-Stanislas <stanislas.thibaud@gmail.com>
2023-04-18 10:56:50 +0200

    calculatrice

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //DOM
-const touche = [...document.querySelectorAll(".bouton")];
-const listeKeycode = touche.map((touche) => touche.dataset.key);
+const touches = [...document.querySelectorAll(".bouton")];
+const listeKeycode = touches.map((touche) => touche.dataset.key);
+const ecran = document.querySelector(".ecran");
 
 document.addEventListener("keydown", (e) => {
   const valeur = e.keycode.toString();
@@ -12,8 +13,20 @@ document.addEventListener("click", (e) => {
   claculer(valeur);
 });
 
-const claculer = (valeur) => {
-    if(listeKeycode.includes(valeur)
-)
+const calculer = (valeur) => {
+  if (listeKeycode.includes(valeur)) {
+    switch (valeur) {
+      case "8":
+        ecran.textContent = "";
+        break;
+      case "13":
+        const calcul = eval(ecran.textContent);
+        ecran.textContent = calcul;
+        break;
+      default:
+        const indexKeycode = listeKeycode.indexOf(valeur);
+        const touche = touches[indexKeycode];
+        ecran.textContent += touche.innerHTML;
+    }
+  }
 };
-   
